c12abde3e8f6c95f85f711fc74a090f8163e88cd
Lina Klass <linaklass@MacBook-Air.local>
2020-06-02 16:13:33 +0200

    added node and tree structure + main + Cmake Lists

---
+++ source/bst.hpp
@@ -0,0 +1,17 @@
+#ifndef BST_HPP 
+#define BST_HPP
+
+class BST{
+    public:
+        //TODO: Search, Minimum, Maximum, Predecessor, Successor, Insert and Delete
+
+        //Print (outputs a .gv file in dot language) 
+            //siehe Folien (auch leere Knoten zeichnen!)
+
+    private:
+        Node root;
+
+
+}
+
+#endif
---
+++ source/node.hpp
@@ -0,0 +1,11 @@
+#ifndef NODE_HPP 
+#define NODE_HPP       
+       
+struct Node {
+    int key_; //stored value
+    Node left_; //left child
+    Node right_; //right child
+    Node p_; //parent
+}       
+
+#endif
